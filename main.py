d5e75d6266a7705db9b45b8ab5c08e0cfcba8e09
careerswitch <abolat01@gmail.com>
2025-06-27 23:58:48 +0300

    Render yaml update

--- main.py
+++ main.py
@@ -194,5 +194,7 @@ async def serve_frontend():
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
